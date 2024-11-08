ec115dd8654336721971785cb87ba138e1a24cdb
dimeh1 <hadimehdi9876@gmail.com>
2024-11-08 11:35:12 +0100

    depuis eclipse

--- src/main/java/com/example/music_app/MusicApplication.java
+++ src/main/java/com/example/music_app/MusicApplication.java
@@ -3,6 +3,9 @@ package com.example.music_app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+//remy
+
 @SpringBootApplication
 public class MusicApplication {
 
