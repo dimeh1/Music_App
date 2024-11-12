a834fcc2ca974ea5aef7d631ba5e9b9761ff26b5
dimeh1 <hadimehdi9876@gmail.com>
2024-11-12 14:52:18 +0100

    modification MusicApplication

--- src/main/java/com/example/music_app/MusicApplication.java
+++ src/main/java/com/example/music_app/MusicApplication.java
@@ -3,9 +3,6 @@ package com.example.music_app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-//remy
-
 @SpringBootApplication
 public class MusicApplication {
 
