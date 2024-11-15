be1506cb424b7f5a92d6e610ac60167823af1d4d
dimeh1 <hadimehdi9876@gmail.com>
2024-11-15 12:59:39 +0100

    Ajout des méthode updatePLaylist et addSongInLike

--- src/main/java/com/example/music_app/controller/PlaylistController.java
+++ src/main/java/com/example/music_app/controller/PlaylistController.java
@@ -1,5 +1,7 @@
 package com.example.music_app.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,8 +9,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.music_app.model.Playlist;
+import com.example.music_app.model.Song;
 import com.example.music_app.model.User;
 import com.example.music_app.repository.PlaylistRepository;
+import com.example.music_app.repository.SongRepository;
 
 import jakarta.servlet.http.HttpSession;
 
@@ -17,6 +21,8 @@ public class PlaylistController {
 	
 	@Autowired
 	PlaylistRepository playlistRepository;
+	@Autowired
+	SongRepository songRepository;
 	
 	@PostMapping("/create-playlist")
 	public String createPLaylist(Model model,@RequestParam("name") String name, HttpSession session ) {
@@ -33,6 +39,7 @@ public class PlaylistController {
 		return "redirect:/";
 	}
 	
+	@PostMapping("/update-playlist")
 	public String updatePlaylist(Model model, @RequestParam("playlistId") Long playlistId, @RequestParam("name") String name, HttpSession session) {
 		if (session.getAttribute("loggedIn") != null && (boolean) session.getAttribute("loggedIn")) {
 			User user = (User) session.getAttribute("user");
@@ -55,4 +62,37 @@ public class PlaylistController {
 		}
 		return "playlist";
 	}
+	
+	@PostMapping("/add-like")
+	public String addSongInLike(Model model, @RequestParam("songId") Long songId, HttpSession session) {
+	    if (session.getAttribute("loggedIn") != null && (boolean) session.getAttribute("loggedIn")) {
+	        
+	        User user = (User) session.getAttribute("user");
+	        
+	        Optional<Song> songOptional = songRepository.findById(songId);
+	        
+	        if (songOptional.isPresent()) {
+	            Song song = songOptional.get();
+	            
+	            Playlist likePlaylist = playlistRepository.findByIdAndUser(1L, user);
+	            
+	            if (likePlaylist != null) {
+	                likePlaylist.getSongs().add(song);
+	                
+	                playlistRepository.save(likePlaylist);
+	                songRepository.save(song);
+	                
+	                model.addAttribute("success", "La chanson a été ajoutée à votre playlist Like.");
+	            } else {
+	                model.addAttribute("error", "Playlist 'Like' introuvable.");
+	            }
+	        } else {
+	            model.addAttribute("error", "Chanson non trouvée.");
+	        }
+	    } else {
+	        model.addAttribute("error", "Vous devez être connecté pour ajouter une chanson à votre playlist.");
+	    }
+	    return "playlist";
+	}
+
 }
