d53ff88aebd14ae333bfd39d50e6ecae0bb41748
MatysChampeyrol <champeyrolmatys.pro@gmail.com>
2024-11-15 13:05:09 +0100

    modification page admin modification controller

--- src/main/java/com/example/music_app/repository/SongRepository.java
+++ src/main/java/com/example/music_app/repository/SongRepository.java
@@ -1,5 +1,7 @@
 package com.example.music_app.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,6 @@ import com.example.music_app.model.Song;
 
 @Repository
 public interface SongRepository extends JpaRepository<Song, Long>{
-
+	Song findByTitre (String titre);
+	Optional<Song> findById(Long id);
 }
