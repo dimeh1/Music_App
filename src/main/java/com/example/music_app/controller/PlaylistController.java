04a44651b3425d407dbad62c7c05b5936fcc722e
dimeh1 <hadimehdi9876@gmail.com>
2024-11-28 19:52:36 +0100

    Ajout images playlists, modification playlist

--- src/main/java/com/example/music_app/controller/PlaylistController.java
+++ src/main/java/com/example/music_app/controller/PlaylistController.java
@@ -66,7 +66,7 @@ public class PlaylistController {
 			Playlist playlist = new Playlist();
 			playlist.setName(name);
 			playlist.setUser(user);
-			
+			playlist.setImage("/img/predefPlaylist.jpeg");			
 			playlistRepository.save(playlist);
 		}
 		
