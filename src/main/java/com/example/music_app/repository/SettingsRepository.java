5a0de9b706bde83930a038095d50e1ace239d644
CYTech Student <mathiassalvage01@gmail.com>
2024-12-13 19:37:20 +0100

    settings fonctionnel. Changement de theme bientot prêt

--- src/main/java/com/example/music_app/repository/SettingsRepository.java
+++ src/main/java/com/example/music_app/repository/SettingsRepository.java
@@ -1,10 +1,13 @@
 package com.example.music_app.repository;
 
-import com.example.music_app.model.Playlist;
 import com.example.music_app.model.Settings;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface SettingsRepository extends JpaRepository<Settings, Long> {
+public interface SettingsRepository extends JpaRepository<Settings, Integer> {
+    // Méthode pour récupérer les paramètres d'un utilisateur spécifique (si nécessaire)
+    Optional<Settings> findById(Integer id);
 }
